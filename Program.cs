df74043bb27575f1148141b008ece2241940c2c8
Paul Oliver <Pauliver@gmail.com>
2020-02-08 06:57:58 -0800

    Should compile now

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Image_Action
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
