c411d8a93c6a3e22ce3130f9d0f62f85b8e4be2f
Paul Oliver <Pauliver@gmail.com>
2020-02-07 18:45:26 -0800

    added 2 files

---
+++ ImageResizer.cs
@@ -0,0 +1,12 @@
+namespace CSharp_Image_Action
+{
+    public class ImageResizer
+    {
+        
+    }
+
+    public static main()
+    {
+        //https://devblogs.microsoft.com/dotnet/net-core-image-processing/
+    }
+}
