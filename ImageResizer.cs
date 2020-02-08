1a062ae967f8fdf5f402fdda20141313f6b23bab
Paul Oliver <Pauliver@gmail.com>
2020-02-08 07:34:53 -0800

    testing

--- ImageResizer.cs
+++ ImageResizer.cs
@@ -1,4 +1,6 @@
-using ImageSharp;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.PixelFormats;
 
 namespace CSharp_Image_Action
 {
@@ -6,6 +8,6 @@ namespace CSharp_Image_Action
     //https://devblogs.microsoft.com/dotnet/net-core-image-processing/
     public class ImageResizer
     {
-        
+        //https://github.com/SixLabors/ImageSharp
     }
 }
